0beeabdaa2f87b115d608150d0c28081f82fca42
henz <henz.jb@gmail.com>
2018-12-29 15:43:13 +0900

    add template file for other questions

---
+++ q0.py
@@ -0,0 +1,13 @@
+"""
+This is template for other questions
+"""
+
+"""
+Main function
+"""
+def main():
+    print("This is template for other questions")
+
+if __name__ == "__main__":
+    main()
+
